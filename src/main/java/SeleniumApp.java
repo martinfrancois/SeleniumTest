1d9dc79d3964050df5226054647548ba0aa0d48b
martinfrancois <fracoisma@hotmail.com>
2017-12-13 10:47:43 +0100

    Initial commit

---
+++ src/main/java/SeleniumApp.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by François Martin on 13.12.17.
+ */
+public class SeleniumApp {
+
+  public static void main(String[] args) {
+    // declaration and instantiation of objects/variables
+    String geckoDriverPath = new File("").getAbsolutePath() + "/geckodriver";
+    System.out.println(geckoDriverPath);
+    System.setProperty("webdriver.gecko.driver", geckoDriverPath);
+    WebDriver driver = new FirefoxDriver();
+    //comment the above 2 lines and uncomment below 2 lines to use Chrome
+    //System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+    //WebDriver driver = new ChromeDriver();
+
+    String baseUrl = "http://demo.guru99.com/selenium/newtours/";
+    String expectedTitle = "Welcome: Mercury Tours";
+    String actualTitle = "";
+
+    // launch Fire fox and direct it to the Base URL
+    driver.get(baseUrl);
+
+    // get the actual value of the title
+    actualTitle = driver.getTitle();
+
+    /*
+     * compare the actual title of the page with the expected one and print
+     * the result as "Passed" or "Failed"
+     */
+    if (actualTitle.contentEquals(expectedTitle)){
+      System.out.println("Test Passed!");
+    } else {
+      System.out.println("Test Failed");
+    }
+
+    //close Fire fox
+    driver.close();
+
+  }
+
+
+}
